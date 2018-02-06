5b6b6ec1faa52bcb36e7fc936a0065da09506e34
Ahto Türkson <ahto.tyrkson@gmail.com>
2018-02-06 17:10:21 +0200

    JUnit tests

---
+++ src/Tests/EdgeTest.java
@@ -0,0 +1,37 @@
+package Tests;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import sample.Edge;
+
+public class EdgeTest{
+    Edge edge;
+
+
+    @Before
+    public void setUp() {
+         edge = new Edge(0, 1, 5);
+    }
+
+    @Test
+    public void testGetVertexFromID() {
+        Assert.assertEquals(edge.getVertexFromID(), 0);
+    }
+
+    @Test
+    public void testGetVertexToID() {
+        Assert.assertEquals(edge.getVertexToID(), 1);
+    }
+
+    @Test
+    public void testGetWeight() {
+        Assert.assertEquals(edge.getWeight(), 5);
+    }
+
+    @Test
+    public void testSetWeight() {
+        edge.setWeight(0);
+        Assert.assertEquals(edge.getWeight(), 0);
+    }
+}
