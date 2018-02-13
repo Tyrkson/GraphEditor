ddc356749465ecd29d3de7fc7d15d3530e72590d
Ahto Türkson <ahto.tyrkson@gmail.com>
2018-02-13 17:22:44 +0200

    Vertex and Edge drawing

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,5 @@
 package sample;
 
-import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
@@ -16,8 +15,14 @@ public class Controller{
 
     //Variables for mode 2 (Edge adding)
     private int clickCount = 0;
-    private Point vertexFromID;
-    private Point vertextToID;
+    private Point pointA;
+    private Point pointB;
+    private int vertexIdA;
+    private int vertexIdB;
+
+
+
+
     private Point vertexCoords;
 
 
@@ -31,19 +36,18 @@ public class Controller{
         if(mode == 1){
             addVertex(e);
         }
-
-        if(mode == 2){
-            addEdge(e);
-        }
     }
 
-    private void addEdge(MouseEvent e){
+    private void addEdge(MouseEvent e, int id){
         if(clickCount == 0){
-            vertexFromID = new Point(e.getX(), e.getY());
+            pointA = new Point(e.getX(), e.getY());
+            vertexIdA = id;
             clickCount++;
         }else{
-            vertextToID = new Point(e.getX(), e.getY());
-            drawLine(vertexFromID, vertextToID);
+            pointB = new Point(e.getX(), e.getY());
+            vertexIdB = id;
+            Edge edge = new Edge(vertexIdA, vertexIdB, 4);
+            drawLine(pointA, pointB);
             clickCount--;
         }
     }
@@ -63,17 +67,26 @@ public class Controller{
     }
 
     public void addVertex(MouseEvent e){
-        //Vertex vertex = new Vertex("Name");
+        Vertex vertex = new Vertex("Name");
         vertexCoords = new Point(e.getX(),e.getY());
-        drawPoint();
+        drawPoint(vertex.getId());
     }
 
-    private void drawPoint(){
+    private void drawPoint(int id){
         Circle c = new Circle();
         c.setFill(Color.BLACK);
         c.setRadius(10);
         c.setCenterX(vertexCoords.getX());
         c.setCenterY(vertexCoords.getY());
+        c.setId(String.valueOf(id));
+
+        c.setOnMouseClicked(e ->{
+            if(mode == 2) {
+                int vertexID = Integer.parseInt(c.getId());
+                addEdge(e, vertexID);
+            }
+        });
+
         drawPane.getChildren().add(c);
     }
     @FXML
@@ -83,6 +96,8 @@ public class Controller{
 
     @FXML
     public void changeModeTo1(){
+        clickCount = 0;
+
         mode = 1;
     }
     @FXML
