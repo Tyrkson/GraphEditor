b47a32ccf025231e07688a0793b084333434a946
Kmarten <marten.kahu@mail.ee>
2018-02-13 16:34:24 +0200

    -

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,36 @@
 package sample;
 
-public class Controller {
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class Controller extends Application{
+    public int createVertex() {
+        Vertex vertex = new Vertex("Name");
+        Graph.addVertex(vertex);
+        return vertex.getId();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("mainWindow.fxml"));
+
+        root.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                System.out.println("Mouse clicked");
+            }
+        });
+
+
+
+        Scene scene = new Scene(root,600,500);
+        stage.setTitle("Graph editor");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
