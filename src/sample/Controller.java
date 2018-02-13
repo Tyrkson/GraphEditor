432c2ce8bff0ef5b8e9d995bbf056b8f922cc8f2
Ahto Türkson <ahto.tyrkson@gmail.com>
2018-02-13 16:33:46 +0200

    Line drawing

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,46 @@
 package sample;
 
-public class Controller {
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
+
+public class Controller{
+
+    private int clickCount = 0;
+    private Point vertexFromID;
+    private Point vertextToID;
+
+    @FXML
+    private Pane drawPane;
+
+
+    @FXML
+    public void addEdge(MouseEvent e){
+        System.out.println(clickCount);
+        if(clickCount == 0){
+            vertexFromID = new Point(e.getX(), e.getY());
+            clickCount++;
+        }else{
+            vertextToID = new Point(e.getX(), e.getY());
+            drawLine();
+            clickCount--;
+        }
+
+
+
+    }
+
+    private void drawLine() {
+        Line l = new Line();
+
+        l.setStartX(vertexFromID.getX());
+        l.setStartY(vertexFromID.getY());
+
+        l.setEndX(vertextToID.getX());
+        l.setEndY(vertextToID.getY());
+
+        drawPane.getChildren().add(l);
+    }
 }
